f090c291f21da3e9659be071503a1a678e1fe330
JK <saikitjk@gmail.com>
2020-09-11 23:28:05 -0700

    created orm.js for selectAll, insertOne, and updateOne queries

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,49 @@
 var connection = require("../config/connection.js");
 
-// `selectAll()`
-// `insertOne()`
-// `updateOne()`
+const orm = {
+  // `selectAll()`
+  selectAll: function (selectObj, targetedTable, cb) {
+    var queryString = "SELECT ?? from ??";
+    connection.query(queryString, [selectObj, targetedTable], function (
+      err,
+      result
+    ) {
+      if (err) {
+        console.error("SQL query error: " + err.stack);
+      }
+      cb(result);
+    });
+  },
+  // `insertOne()`
+  insertOne: function (targetedTable, burgerName, isDevoured, cb) {
+    var queryString =
+      "INSERT INTO ?? (burger_name, devoured) VALUE ('??','??')";
+    connection.query(
+      queryString,
+      [targetedTable, burgerName, isDevoured],
+      function (err, result) {
+        if (err) {
+          console.error("SQL query error: " + err.stack);
+        }
+        cb(result);
+      }
+    );
+  },
+  // `updateOne()`
+
+  updateOne: function (targetedTable, isDevoured, burgerName, cb) {
+    var queryString = "UPDATE ?? SET ?? WHERE ??";
+    connection.query(
+      queryString,
+      [targetedTable, isDevoured, burgerName],
+      function (err, result) {
+        if (err) {
+          console.error("SQL query error: " + err.stack);
+        }
+        cb(result);
+      }
+    );
+  },
+};
+
+module.exports = orm;
