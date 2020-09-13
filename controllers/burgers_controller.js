ac0680099a6c9c7c81235a7a20656917856744b2
JK <saikitjk@gmail.com>
2020-09-12 23:55:18 -0700

    fixed some errors

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,26 +22,26 @@ router.post("/api/burgers", function (req, res) {
       res.json({ id: result.insertId });
     }
   );
+});
 
-  router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
 
-    console.log("Condition", condition);
+  console.log("Condition", condition);
 
-    burger.update(
-      {
-        isDevoured: req.body.isDevoured,
-      },
-      condition,
-      function (data) {
-        if (data.dataRow === 0) {
-          return res.status(404).end();
-        } else {
-          res.status(200).end(0);
-        }
+  burger.update(
+    {
+      isDevoured: req.body.isDevoured,
+    },
+    condition,
+    function (data) {
+      if (data.dataRow === 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end(0);
       }
-    );
-  });
+    }
+  );
 });
 /////
 
