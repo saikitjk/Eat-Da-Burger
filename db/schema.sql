bb97493b20f4e7a7b4f56434eb5f753cff5fdcfb
JK <saikitjk@gmail.com>
2020-09-15 14:43:45 -0700

    updated the schema and seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,13 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
+USE burgers_db;
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50),
-    devoured BOOLEAN,
+    burger_name VARCHAR(250),
+    devoured Boolean,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
-)
+) -- UPDATE burgers SET devoured = 	True where id in (21)
+- -
+DELETE FROM BURGERS
+WHERE id > 24 -- SELECT * FROM burgers
--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,9 @@ INSERT INTO burgers (burger_name, devoured)
 VALUES("Cheese Burger", FALSE);
 INSERT INTO burgers (burger_name, devoured)
 VALUES("Quarter Pounder", FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES('Filet-O-Fish', FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES('McChicken', TRUE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES('JK''s special', FALSE);
