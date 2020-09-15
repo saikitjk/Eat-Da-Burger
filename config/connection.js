36fae2fc010df80e46eacc8d08800a4e7e3fd025
JK <saikitjk@gmail.com>
2020-09-15 13:56:20 -0700

    added JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const util = require("util");
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "P@ssword!",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "P@ssword!",
+    database: "burgers_db",
+  });
+}
 
 connection.connect((err) => {
   if (err) {
